32d2c3d1a6ff51d6ff86c0dd56b19e5c4d564626
charlesstw <charlesstw@hotmail.com>
2020-04-15 19:05:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   console.log("Charles test");
-  var locationJson = '{"location":"34.9671402,135.7726717"}';
+//  var locationJson = '{"location":"34.9671402,135.7726717"}';
+  var locationJson = '{"location":"https://goo.gl/maps/3gTG4Jt9VCTquHTP9"}';
   window.webkit.messageHandlers.readium.postMessage(['googleStreetView', locationJson]);
-  window.location.href = "https://goo.gl/maps/3gTG4Jt9VCTquHTP9";
 };

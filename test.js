41981aa87035bc4a6fe4a9d96ecd2dbe76953b61
charlesstw <charlesstw@hotmail.com>
2020-04-15 18:19:40 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 window.onload = function() {
   console.log("Charles test");
-  var locationJson = {"location":"34.967149,135.772677"};
+  var locationJson = '{"location":"34.967149,135.772677"}';
   window.webkit.messageHandlers.readium.postMessage(['googleStreetView', locationJson]);
 };

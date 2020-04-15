6994d6e70e23e71c1ea8e9ea692dcb2325acf946
charlesstw <charlesstw@hotmail.com>
2020-04-15 14:52:31 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert("laoded javascript!!");
+$(document).ready(function(){
+  console.log("Charles test");
+  var location = "34.967149,135.772677";
+  window.webkit.messageHandlers.readium.postMessage(['googleStreetView', location]);
+});

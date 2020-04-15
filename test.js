039788c8140049b65528aac1e5b5e458fd7a0868
charlesstw <charlesstw@hotmail.com>
2020-04-15 18:54:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   console.log("Charles test");
-  var locationJson = '{"location":"35.010327,135.666977"}';
+  var locationJson = '{"location":"34.9671402,135.7726717"}';
   window.webkit.messageHandlers.readium.postMessage(['googleStreetView', locationJson]);
+  window.location.href = "https://goo.gl/maps/3gTG4Jt9VCTquHTP9";
 };
